14cc19be28e8ba186ea2afb45f39cad2e62dca72
sanyagame98 <nadone229@gmail.com>
2026-08-14 13:44:55 +0500

    Add Roof-native TWeb bootstrap responses

--- roof-tweb/roofTransport.ts
+++ roof-tweb/roofTransport.ts
@@ -19,6 +19,12 @@ function getWsBase(): string {
   return url.toString();
 }
 
+function cancellableResolved<T>(value: T): RoofCancellablePromise<T> {
+  const promise = Promise.resolve(value) as RoofCancellablePromise<T>;
+  promise.cancel = () => {};
+  return promise;
+}
+
 class RoofTransport {
   private updateSocket?: WebSocket;
   private reconnectTimer?: number;
@@ -91,11 +97,133 @@ class RoofTransport {
     }).then((result) => this.rememberAuth(result));
   }
 
+  private localBootstrap(method: string): unknown | undefined {
+    const now = Math.floor(Date.now() / 1000);
+
+    if(method === 'help.getConfig') {
+      return {
+        _: 'config',
+        date: now,
+        expires: now + 86400,
+        test_mode: false,
+        this_dc: 1,
+        dc_options: [],
+        dc_txt_domain_name: 'roof.local',
+        chat_size_max: 200,
+        megagroup_size_max: 200000,
+        forwarded_count_max: 100,
+        online_update_period_ms: 120000,
+        offline_blur_timeout_ms: 5000,
+        offline_idle_timeout_ms: 30000,
+        online_cloud_timeout_ms: 300000,
+        notify_cloud_delay_ms: 30000,
+        notify_default_delay_ms: 1500,
+        push_chat_period_ms: 60000,
+        push_chat_limit: 2,
+        saved_gifs_limit: 200,
+        edit_time_limit: 172800,
+        revoke_time_limit: 172800,
+        revoke_pm_time_limit: 172800,
+        rating_e_decay: 2419200,
+        stickers_recent_limit: 200,
+        channels_read_media_period: 604800,
+        tmp_sessions: 0,
+        call_receive_timeout_ms: 20000,
+        call_ring_timeout_ms: 90000,
+        call_connect_timeout_ms: 30000,
+        call_packet_timeout_ms: 10000,
+        me_url_prefix: 'roof://',
+        autoupdate_url_prefix: '',
+        gif_search_username: '',
+        venue_search_username: '',
+        img_search_username: '',
+        static_maps_provider: '',
+        caption_length_max: 4096,
+        message_length_max: 4096,
+        webfile_dc_id: 1,
+        suggested_lang_code: 'en',
+        lang_pack_version: 0,
+        base_lang_pack_version: 0,
+        pFlags: {}
+      };
+    }
+
+    if(method === 'help.getAppConfig') {
+      return {
+        _: 'help.appConfig',
+        hash: 1,
+        config: {
+          dialogs_pinned_limit_default: 5,
+          dialogs_pinned_limit_premium: 10,
+          dialogs_folder_pinned_limit_default: 5,
+          dialogs_folder_pinned_limit_premium: 10,
+          dialog_filters_limit_default: 10,
+          dialog_filters_limit_premium: 20,
+          stickers_faved_limit_default: 5,
+          stickers_faved_limit_premium: 10,
+          reactions_user_max_default: 1,
+          reactions_user_max_premium: 3,
+          about_length_limit_default: 280,
+          about_length_limit_premium: 280,
+          topics_pinned_limit: 5,
+          caption_length_limit_default: 4096,
+          caption_length_limit_premium: 4096,
+          chatlist_invites_limit_default: 3,
+          chatlist_invites_limit_premium: 10,
+          chatlists_joined_limit_default: 2,
+          chatlists_joined_limit_premium: 20,
+          channels_limit_default: 500,
+          channels_limit_premium: 1000,
+          channels_public_limit_default: 10,
+          channels_public_limit_premium: 20,
+          saved_gifs_limit_default: 200,
+          saved_gifs_limit_premium: 400,
+          dialog_filters_chats_limit_default: 100,
+          dialog_filters_chats_limit_premium: 200,
+          upload_max_fileparts_default: 4000,
+          upload_max_fileparts_premium: 8000,
+          recommended_channels_limit_default: 10,
+          recommended_channels_limit_premium: 20,
+          saved_dialogs_pinned_limit_default: 5,
+          saved_dialogs_pinned_limit_premium: 10,
+          ignore_restriction_reasons: []
+        }
+      };
+    }
+
+    if(method === 'help.getTimezonesList') {
+      return {_: 'help.timezonesList', timezones: [], hash: 1};
+    }
+
+    if(method === 'help.getPeerColors' || method === 'help.getPeerProfileColors') {
+      return {_: 'help.peerColors', colors: [], hash: 1};
+    }
+
+    if(method === 'contacts.getStatuses') return [];
+    if(method === 'messages.getTopPeers') {
+      return {_: 'contacts.topPeers', categories: [], chats: [], users: []};
+    }
+    if(method === 'messages.getSavedDialogs') {
+      return {_: 'messages.savedDialogs', dialogs: [], messages: [], chats: [], users: []};
+    }
+    if(method === 'account.getGlobalPrivacySettings') {
+      return {_: 'globalPrivacySettings', pFlags: {}};
+    }
+    if(method === 'account.getContentSettings') {
+      return {_: 'account.contentSettings', pFlags: {}};
+    }
+
+    return undefined;
+  }
+
   public invoke<T = unknown>(
     method: string,
     params: RoofInvokeParams = {},
     _options: RoofInvokeOptions = {}
   ): RoofCancellablePromise<T> {
+    const local = this.localBootstrap(method);
+    if(local !== undefined) return cancellableResolved(local as T);
+
     const controller = new AbortController();
     const promise = this.request<T>(
       '/roof/invoke',
